c15a853f11cf550d77d183ddddab611b41ec210b
francineochoafernandez <francineochoaf@outlook.com>
2023-10-28 23:56:16 -0600

    Pruebas iniciales

--- app/src/main/java/com/francineochoa/formulasoficial/MainActivity.kt
+++ app/src/main/java/com/francineochoa/formulasoficial/MainActivity.kt
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
+import com.francineochoa.formulasoficial.databinding.ActivityMainBinding
 import com.francineochoa.formulasoficial.databinding.Diseno1Binding
 
 class MainActivity : AppCompatActivity() {
 
     //Promesa de inicializar la variable despues
-    private lateinit var binding: Diseno1Binding
+    private lateinit var binding: Diseno1Binding //ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,8 +38,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     //Funcion que corre cuando se le da click al boton
-    fun clickEmpecemos(view: View){
-        binding.tvTitulo.text ="¡Vamos!"
+    fun clickCalcular(view: View){
+
+        if( binding.editTextNumber1.text.isNotEmpty() &&  binding.editTextNumber2.text.isNotEmpty()  &&  binding.editTextNumber3.text.isNotEmpty() )
+        {
+            val numero1 = binding.editTextNumber1.text.toString().toInt()
+            val numero2 = binding.editTextNumber2.text.toString().toInt()
+            val numero3 = binding.editTextNumber3.text.toString().toInt()
+            val resultado=numero1+numero2+numero3
+            binding.textViewResultado.text="Resultado= $resultado"
+        }
+        else
+        {
+            Toast.makeText(this,"Por favor ingresa todos los numeros",Toast.LENGTH_LONG).show()
+        }
+
+
+
+
+
 
     }
 
